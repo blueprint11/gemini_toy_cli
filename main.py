84bde18b627a3c8e272e8dcd8b4aaf8706543288
blueprint11 <goyalakshay02gmail.com>
2025-07-08 09:12:10 +0000

    Added command line argument support with sys.argv and error handling for missing prompt

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 def main():
+
+    if len(sys.argv) < 2:
+        print("error: no prompt given!")
+        sys.exit(1)
+    elif len(sys.argv) == 2:
+        prompt = sys.argv[1]
+    
+    else:
+        print("error: need to provide prompt in double braces")
+        sys.exit(1)
+
+    
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     
@@ -9,7 +22,7 @@ def main():
     print("Hello from toyccai!")
     response = client.models.generate_content(
     model='gemini-2.0-flash-001', 
-    contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    contents=prompt
     )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
