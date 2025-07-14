6c64aa90d89d8c97ead39e48a58d2ff9ae0be4e3
blueprint11 <goyalakshay02gmail.com>
2025-07-14 11:36:37 +0000

    adding schemas for write, read , get contents functionalities. modified system prompt

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import sys
 from google.genai import types
 from prompts import system_prompt
 from call_function import available_functions
-
 def main():
 
     if len(sys.argv) < 2:
@@ -34,23 +33,23 @@ def main():
     client = genai.Client(api_key=api_key)
     print("Hello from toyccai!")
     response = client.models.generate_content(
-    model='gemini-2.0-flash-001', 
-    contents=messages,
-    config=types.GenerateContentConfig(
-            tools=[available_functions], system_instruction=system_prompt
-        ),
+                model='gemini-2.0-flash-001', 
+                contents=messages,
+                config=types.GenerateContentConfig(
+                tools=[available_functions],system_instruction=system_prompt
+                ),
     )
-    
-    if len(sys.argv)==3 and sys.argv[2]=="--verbose":
-        prompt =sys.argv[1]
-        print(f"User prompt: {prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     if not response.function_calls:
         return response.text
 
     for function_call_part in response.function_calls:
         print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+
+    if len(sys.argv)==3 and sys.argv[2]=="--verbose":
+        prompt =sys.argv[1]
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
